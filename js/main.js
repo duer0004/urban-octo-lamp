42451073d2342fbd72b620fb35fd20a006454e74
Xavier Duerden <duer0004@algonquinlive.com>
2024-10-04 10:56:17 -0400

    new buildContent2 function added

--- js/main.js
+++ js/main.js
@@ -16,8 +16,26 @@ function buildContent(){
     //assign that one string as the innerHTML of the main tag
 }
 
-function clearContent(){ //clears all the content in the main tag
+function buildContent2(){
+    const chunk = document.createDocumentFragment();
+    //chunk is a temporary container for HTML elements
+    characters.forEach(function(char){
+        let section= document.createElement('section'); //HTML section object
+        let h2= document.createElement('h2'); //HTML Heading 2 object
+        let p= document.createElement('p'); //HTML Paragraph object
+        section.className = 'highlight'; //add a class to the section
+        section.dataset.ref = char.uuid;
+        h2.textContent = char.name;
+        p.textContent = `${char.role} - ${char.personality}`;
+        chunk.append(section);
+        section.append(h2, p);
+    });
+    document.querySelector('main').append(chunk);
+}
+function clearContent(){
     document.querySelector('main').innerHTML = '';
 }
+
 clearContent();
 buildContent();
+buildContent2();
